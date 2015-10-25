4eaddecdc629606453df3b683dea405da6e6aeb8
mkuchin <mkuchin@gmail.com>
2015-10-25 23:33:00 +1100

    build and release fix

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 . settings
 . runon.sh $BUILD_NODE
 APP=$1
-APP_NAME=REGISTRY_HOST/$1
-docker build -t $APP_NAME $(cat "apps/$APP")
+APP_NAME=$REGISTRY_HOST/$APP
+GIT_REPO=$(cat "apps/$APP")
+docker build -t $APP_NAME $GIT_REPO
 docker push $APP_NAME
 
--- release.sh
+++ release.sh
@@ -1,6 +1,10 @@
 NODE=$1
 APP=$2
+echo Building
 ./build.sh $APP
+echo Pulling image
 ./pull-image.sh $NODE $APP
+echo Stopping service
 ./stop-service.sh $NODE $APP
+echo Starting service
 ./start-service.sh $NODE $APP
